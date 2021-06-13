a3a7234fd214b9ed45adf0512f6b6089b7706f56
mohdAzzam <alazzamhmode@gmail.com>
2021-06-13 16:52:23 +0300

    lab01 react done

---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.scss';
+
+
+const Header = ()=>{
+    return (
+        <header>
+            <h1>RESTy</h1>
+        </header>
+    )
+}
+
+export default Header;
